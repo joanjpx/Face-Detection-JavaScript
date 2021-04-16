98596bdf65c1047418521011cc5e988063494f12
Joan Perez <joansyn.s@gmail.com>
2021-04-15 20:45:26 -0500

    Log Detections

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ video.addEventListener('play', () => {
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    console.log(detections);
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
